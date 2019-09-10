71cf24722ca8263d5d4a1dd385ca3b955aef91fc
kateharborne <katherine.harborne@icrar.org>
2019-09-10 10:16:37 +0800

    Making modifications to allow for the new method of comuting luminosities using ProSpect.

--- R/obs_data_prep.R
+++ R/obs_data_prep.R
@@ -46,61 +46,60 @@
 #'
 
 obs_data_prep = function(simdata, r200=200, z=0.05, fov=15, ap_shape="circular", central_wvl=4800,
-                         lsf_fwhm=2.65, pixel_sscale=0.5, pixel_vscale=1.04, inc_deg=70){
+                         lsf_fwhm=2.65, pixel_sscale=0.5, pixel_vscale=1.04, inc_deg=70, align=TRUE){
 
   set.seed(42);
-  sol_lum = 3.827e33;                                      # solar luminosity in erg s-1
+  sol_lum = 3.827e33;                                        # solar luminosity in erg s-1
 
-  ang_size     = celestial::cosdistAngScale(z, ref="Planck")
-  # angular size given z, kpc/"
-  lum_dist     = celestial::cosdistLumDist(z, ref="Planck")
-  # the luminosity distance, Mpc
-  ap_size      = ang_size * fov                            # diameter size of the telescope, kpc
-  sbin         = floor(fov / pixel_sscale)                 # bin sizes in the x- & y/z_obs- axes
-  sbinsize     = ap_size / sbin                            # kpc per bin
-  vbinsize     = (pixel_vscale / central_wvl) * (3e8 / 1e3)
-  # km/s per velocity bin
-  lsf_size     = ((lsf_fwhm / central_wvl) * (3e8 / 1e3)) / (2 * sqrt(2*log(2)))
-  # velocity uncertainty (sd)
+  ang_size     = celestial::cosdistAngScale(z, ref="Planck") # angular size given z, kpc/"
+  lum_dist     = celestial::cosdistLumDist(z, ref="Planck")  # the luminosity distance, Mpc
+  ap_size      = ang_size * fov                              # diameter size of the telescope, kpc
+  sbin         = floor(fov / pixel_sscale)                   # bin sizes in the x- & y/z_obs- axes
+  sbinsize     = ap_size / sbin                              # kpc per bin
+  vbinsize     = (pixel_vscale / central_wvl) * (3e8 / 1e3)  # km/s per velocity bin
+  lsf_size     = ((lsf_fwhm / central_wvl) * (3e8 / 1e3)) / (2 * sqrt(2*log(2))) # velocity uncertainty (sd)
 
   if (ap_shape == "circular"){
     ap_region = .circular_ap(sbin)
-  } # circular apperture mask
+  }  # circular apperture mask
   if (ap_shape == "square"){
     ap_region = matrix(data = 1, ncol = sbin, nrow = sbin)
-  } # square apperture mask
+  }    # square apperture mask
   if (ap_shape == "hexagonal"){
     ap_region = .hexagonal_ap(sbin)
   } # hexagonal apperture mask
 
   result = grepl(paste(c("PartType2", "PartType3", "PartType4"), collapse = "|"), names(simdata))
-  SSP = FALSE
   # finding the luminous matter within the simulation for imaging
+  #SSP = FALSE # placeholder for SSP for loop, remains FALSE if no SSP in simdata
 
-  if (any(result)) {                                       # concatenating the seperate particle
-    present = which(result)                                #  data.frames into a single data.frame
+  if (any(result)) {
+    present = which(result)
     if (length(present) == 1){
       galaxy_data = simdata[[present[1]]]$Part
-      if ("SSP" %in% names(simdata[[present[1]]])){
-        SSP = which(grepl("SSP", names(simdata[[present[1]]])))}
+      # if ("SSP" %in% names(simdata[[present[1]]])){
+      #   SSP = which(grepl("SSP", names(simdata[[present[1]]])))}
     } else if (length(present) == 2){
       galaxy_data = rbind(simdata[[present[1]]]$Part, simdata[[present[2]]]$Part)
-      if (any("SSP" %in% c(names(simdata[[present[1]]]), names(simdata[[present[2]]])))){
-        SSP = which(grepl("SSP", c(names(simdata[[present[1]]]), names(simdata[[present[2]]]))))}
+      # if (any("SSP" %in% c(names(simdata[[present[1]]]), names(simdata[[present[2]]])))){
+      #   SSP = which(grepl("SSP", c(names(simdata[[present[1]]]), names(simdata[[present[2]]]))))}
     } else if (length(present) == 3){
       galaxy_data = rbind(simdata[[present[1]]]$Part, simdata[[present[2]]]$Part, simdata[[present[3]]]$Part)
-      if (any("SSP" %in% c(names(simdata[[present[1]]]), names(simdata[[present[2]]]), names(simdata[[present[3]]])))){
-        SSP = which(grepl("SSP", c(names(simdata[[present[1]]]), names(simdata[[present[2]]]), names(simdata[[present[3]]]))))}
+      # if (any("SSP" %in% c(names(simdata[[present[1]]]), names(simdata[[present[2]]]), names(simdata[[present[3]]])))){
+      #   SSP = which(grepl("SSP", c(names(simdata[[present[1]]]), names(simdata[[present[2]]]), names(simdata[[present[3]]]))))}
     }
   } else {
     cat("There are no particles representing luminous matter in this simulation (i.e. no stars, bulge or disc particles). \n")
     stop("LumPart Error") # if no stars, bulge or disc component, stop trying to build IFU cube
   }
+  # concatenating the seperate ptype into a single particle data frame
 
-  galaxy_df   = obs_galaxy(galaxy_data, centre=TRUE, inc_deg * (pi / 180))
-  # extracting the position and LOS data
-  galaxy_df   = galaxy_df[(galaxy_df$r < r200),]
-  # removing particles beyond r200
+  galaxy_data = cen_galaxy(galaxy_data) # centre data
+  if (align){
+    galaxy_data = .reorient_galaxy(galaxy_data) # reorient galaxies such that the semimajor axis lies along the x
+  }
+  galaxy_df   = obs_galaxy(galaxy_data, inc_deg * (pi / 180)) # extracting the position and LOS data
+  galaxy_df   = galaxy_df[(galaxy_df$r < r200),] # removing particles beyond r200
 
   if (ap_shape == "circular"){
     galaxy_cdf  = .circular_ap_cut(galaxy_df, ap_size)
@@ -119,44 +118,82 @@ obs_data_prep = function(simdata, r200=200, z=0.05, fov=15, ap_shape="circular",
   # vseq = seq(-(vbin * vbinsize) / 2, (vbin * vbinsize) / 2, by=vbinsize)    # velocity bin break positions
 
   galaxy_cdf$binn = cut(galaxy_cdf$x, breaks=sseq, labels=F) +
-    (sbin * cut(galaxy_cdf$z_obs, breaks=sseq, labels=F)) - sbin                           # assigning particles to positions in aperture
+    (sbin * cut(galaxy_cdf$z_obs, breaks=sseq, labels=F)) - sbin              # assigning particles to positions in aperture
+
+  if (length(present) == 1){
+    if ("SSP" %in% names(simdata[[present[1]]])){
+      galaxy_cdf$Metallicity = simdata[[present[1]]]$SSP[simdata[[present[1]]]$Part$ID %in% galaxy_cdf$ID,]$Metallicity
+      galaxy_cdf$Age         = simdata[[present[1]]]$SSP[simdata[[present[1]]]$Part$ID %in% galaxy_cdf$ID,]$Age
+    }
+    else if ("Lum" %in% names(simdata[[present[1]]])){
+      galaxy_cdf$Lum = simdata[[present[1]]]$Lum[as.integer(simdata[[present[1]]]$Part$ID) %in% as.integer(galaxy_cdf$ID)]
+    }
+  } else if (length(present) == 2){
+      if (all("SSP" %in% c(names(simdata[[present[1]]]), names(simdata[[present[2]]])))){
+        galaxy_cdf$Metallicity = c(simdata[[present[1]]]$SSP[simdata[[present[1]]]$Part$ID %in% galaxy_cdf$ID,]$Metallicity,
+                                   simdata[[present[2]]]$SSP[simdata[[present[2]]]$Part$ID %in% galaxy_cdf$ID,]$Metallicity)
+        galaxy_cdf$Age         = c(simdata[[present[1]]]$SSP[simdata[[present[1]]]$Part$ID %in% galaxy_cdf$ID,]$Age,
+                                   simdata[[present[2]]]$SSP[simdata[[present[2]]]$Part$ID %in% galaxy_cdf$ID,]$Age)
+
+      }
+      else if (all("Lum" %in% c(names(simdata[[present[1]]]), names(simdata[[present[2]]])))){
+        galaxy_cdf$Lum = c(simdata[[present[1]]]$Lum[as.integer(simdata[[present[1]]]$Part$ID) %in% as.integer(galaxy_cdf$ID)],
+                           simdata[[present[2]]]$Lum[as.integer(simdata[[present[2]]]$Part$ID) %in% as.integer(galaxy_cdf$ID)])
+      }
+  } else if (length(present) == 3){
+      if (all("SSP" %in% c(names(simdata[[present[1]]]), names(simdata[[present[2]]]), names(simdata[[present[3]]])))){
+        galaxy_cdf$Metallicity = c(simdata[[present[1]]]$SSP[simdata[[present[1]]]$Part$ID %in% galaxy_cdf$ID,]$Metallicity,
+                                   simdata[[present[2]]]$SSP[simdata[[present[2]]]$Part$ID %in% galaxy_cdf$ID,]$Metallicity,
+                                   simdata[[present[3]]]$SSP[simdata[[present[3]]]$Part$ID %in% galaxy_cdf$ID,]$Metallicity)
+        galaxy_cdf$Age         = c(simdata[[present[1]]]$SSP[simdata[[present[1]]]$Part$ID %in% galaxy_cdf$ID,]$Age,
+                                   simdata[[present[2]]]$SSP[simdata[[present[2]]]$Part$ID %in% galaxy_cdf$ID,]$Age,
+                                   simdata[[present[3]]]$SSP[simdata[[present[3]]]$Part$ID %in% galaxy_cdf$ID,]$Age)
+
+      }
+      else if (all("Lum" %in% c(names(simdata[[present[1]]]), names(simdata[[present[2]]]), names(simdata[[present[3]]])))){
+        galaxy_cdf$Lum = c(simdata[[present[1]]]$Lum[as.integer(simdata[[present[1]]]$Part$ID) %in% as.integer(galaxy_cdf$ID)],
+                           simdata[[present[2]]]$Lum[as.integer(simdata[[present[2]]]$Part$ID) %in% as.integer(galaxy_cdf$ID)],
+                           simdata[[present[3]]]$Lum[as.integer(simdata[[present[3]]]$Part$ID) %in% as.integer(galaxy_cdf$ID)])
+      }
+  }
+  # adding the relevant Lum/SSP info to the trimmed particles for later processing
 
   # If SSP is present, assign relevant Age/Metallicities. Else, assign luminoisty.
-  if (any(as.logical(SSP))){
-    if (length(SSP) == 1){
-      inc_ID = which(simdata[[present[1]]]$Part$ID %in% galaxy_cdf$ID)
-      galaxy_cdf$Age = simdata[[present[1]]]$SSP$Age[inc_ID] * 1e9
-      galaxy_cdf$Metallicity = simdata[[present[1]]]$SSP$Metallicity[inc_ID]
-    } else if (length(SSP) == 2){
-      inc_ID_1 = which(simdata[[present[1]]]$Part$ID %in% galaxy_cdf$ID)
-      inc_ID_2 = which(simdata[[present[2]]]$Part$ID %in% galaxy_cdf$ID)
-      galaxy_cdf$Age = c(simdata[[present[1]]]$SSP$Age[inc_ID_1] * 1e9, simdata[[present[2]]]$SSP$Age[inc_ID_2] * 1e9)
-      galaxy_cdf$Metallicity = c(simdata[[present[1]]]$SSP$Metallicity[inc_ID_1], simdata[[present[2]]]$SSP$Metallicity[inc_ID_2])
-    } else if (length(SSP) == 3){
-      inc_ID_1 = which(simdata[[present[1]]]$Part$ID %in% galaxy_cdf$ID)
-      inc_ID_2 = which(simdata[[present[2]]]$Part$ID %in% galaxy_cdf$ID)
-      inc_ID_3 = which(simdata[[present[3]]]$Part$ID %in% galaxy_cdf$ID)
-      galaxy_cdf$Age = c(simdata[[present[1]]]$SSP$Age[inc_ID_1] * 1e9, simdata[[present[2]]]$SSP$Age[inc_ID_2] * 1e9, simdata[[present[3]]]$SSP$Age[inc_ID_3] * 1e9)
-      galaxy_cdf$Metallicity = c(simdata[[present[1]]]$SSP$Metallicity[inc_ID_1], simdata[[present[2]]]$SSP$Metallicity[inc_ID_2], simdata[[present[3]]]$SSP$Metallicity[inc_ID_3])}
-  } else {
-    if (length(present) == 1){
-      inc_ID = which(simdata[[present[1]]]$Part$ID %in% galaxy_cdf$ID)
-      galaxy_cdf$flux = (simdata[[present[1]]]$Lum[inc_ID] * sol_lum * (pixel_sscale^2)) / (1e-16 * 4 * pi * (lum_dist * 3.086e24)^2)
-    } else if (length(present) == 2){
-      inc_ID_1 = which(simdata[[present[1]]]$Part$ID %in% galaxy_cdf$ID)
-      inc_ID_2 = which(simdata[[present[2]]]$Part$ID %in% galaxy_cdf$ID)
-      galaxy_cdf$flux = c((simdata[[present[1]]]$Lum[inc_ID_1] * sol_lum * (pixel_sscale^2)) / (1e-16 * 4 * pi * (lum_dist * 3.086e24)^2),
-                          (simdata[[present[2]]]$Lum[inc_ID_2] * sol_lum * (pixel_sscale^2)) / (1e-16 * 4 * pi * (lum_dist * 3.086e24)^2))
-    } else if (length(present) == 3){
-      inc_ID_1 = which(simdata[[present[1]]]$Part$ID %in% galaxy_cdf$ID)
-      inc_ID_2 = which(simdata[[present[2]]]$Part$ID %in% galaxy_cdf$ID)
-      inc_ID_3 = which(simdata[[present[3]]]$Part$ID %in% galaxy_cdf$ID)
-      galaxy_cdf$flux = c((simdata[[present[1]]]$Lum[inc_ID_1] * sol_lum * (pixel_sscale^2)) / (1e-16 * 4 * pi * (lum_dist * 3.086e24)^2),
-                          (simdata[[present[2]]]$Lum[inc_ID_2] * sol_lum * (pixel_sscale^2)) / (1e-16 * 4 * pi * (lum_dist * 3.086e24)^2),
-                          (simdata[[present[3]]]$Lum[inc_ID_3] * sol_lum * (pixel_sscale^2)) / (1e-16 * 4 * pi * (lum_dist * 3.086e24)^2))
-
-    }
-  }
+  #  if (any(as.logical(SSP))){
+  #   if (length(SSP) == 1){
+  #     inc_ID = which(simdata[[present[1]]]$Part$ID %in% galaxy_cdf$ID)
+  #     galaxy_cdf$Age = simdata[[present[1]]]$SSP$Age[inc_ID] * 1e9
+  #     galaxy_cdf$Metallicity = simdata[[present[1]]]$SSP$Metallicity[inc_ID]
+  #   } else if (length(SSP) == 2){
+  #     inc_ID_1 = which(simdata[[present[1]]]$Part$ID %in% galaxy_cdf$ID)
+  #     inc_ID_2 = which(simdata[[present[2]]]$Part$ID %in% galaxy_cdf$ID)
+  #     galaxy_cdf$Age = c(simdata[[present[1]]]$SSP$Age[inc_ID_1] * 1e9, simdata[[present[2]]]$SSP$Age[inc_ID_2] * 1e9)
+  #     galaxy_cdf$Metallicity = c(simdata[[present[1]]]$SSP$Metallicity[inc_ID_1], simdata[[present[2]]]$SSP$Metallicity[inc_ID_2])
+  #   } else if (length(SSP) == 3){
+  #     inc_ID_1 = which(simdata[[present[1]]]$Part$ID %in% galaxy_cdf$ID)
+  #     inc_ID_2 = which(simdata[[present[2]]]$Part$ID %in% galaxy_cdf$ID)
+  #     inc_ID_3 = which(simdata[[present[3]]]$Part$ID %in% galaxy_cdf$ID)
+  #     galaxy_cdf$Age = c(simdata[[present[1]]]$SSP$Age[inc_ID_1] * 1e9, simdata[[present[2]]]$SSP$Age[inc_ID_2] * 1e9, simdata[[present[3]]]$SSP$Age[inc_ID_3] * 1e9)
+  #     galaxy_cdf$Metallicity = c(simdata[[present[1]]]$SSP$Metallicity[inc_ID_1], simdata[[present[2]]]$SSP$Metallicity[inc_ID_2], simdata[[present[3]]]$SSP$Metallicity[inc_ID_3])}
+  # } else {
+  #   if (length(present) == 1){
+  #     inc_ID = which(simdata[[present[1]]]$Part$ID %in% galaxy_cdf$ID)
+  #     galaxy_cdf$flux = (simdata[[present[1]]]$Lum[inc_ID] * sol_lum * (pixel_sscale^2)) / (1e-16 * 4 * pi * (lum_dist * 3.086e24)^2)
+  #   } else if (length(present) == 2){
+  #     inc_ID_1 = which(simdata[[present[1]]]$Part$ID %in% galaxy_cdf$ID)
+  #     inc_ID_2 = which(simdata[[present[2]]]$Part$ID %in% galaxy_cdf$ID)
+  #     galaxy_cdf$flux = c((simdata[[present[1]]]$Lum[inc_ID_1] * sol_lum * (pixel_sscale^2)) / (1e-16 * 4 * pi * (lum_dist * 3.086e24)^2),
+  #                         (simdata[[present[2]]]$Lum[inc_ID_2] * sol_lum * (pixel_sscale^2)) / (1e-16 * 4 * pi * (lum_dist * 3.086e24)^2))
+  #   } else if (length(present) == 3){
+  #     inc_ID_1 = which(simdata[[present[1]]]$Part$ID %in% galaxy_cdf$ID)
+  #     inc_ID_2 = which(simdata[[present[2]]]$Part$ID %in% galaxy_cdf$ID)
+  #     inc_ID_3 = which(simdata[[present[3]]]$Part$ID %in% galaxy_cdf$ID)
+  #     galaxy_cdf$flux = c((simdata[[present[1]]]$Lum[inc_ID_1] * sol_lum * (pixel_sscale^2)) / (1e-16 * 4 * pi * (lum_dist * 3.086e24)^2),
+  #                         (simdata[[present[2]]]$Lum[inc_ID_2] * sol_lum * (pixel_sscale^2)) / (1e-16 * 4 * pi * (lum_dist * 3.086e24)^2),
+  #                         (simdata[[present[3]]]$Lum[inc_ID_3] * sol_lum * (pixel_sscale^2)) / (1e-16 * 4 * pi * (lum_dist * 3.086e24)^2))
+  #
+  #   }
+  # }
 
   # tic()
   # ID_grid = vector("list", sbin*sbin)
