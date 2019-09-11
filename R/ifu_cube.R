0ee36c4db0d2048f55060d0d4e5815c4fe73c8a4
kateharborne <katherine.harborne@icrar.org>
2019-09-11 17:52:10 +0800

    Significant speed up of ifu_cube. Now uses the output from img_grid() for a more concise loop. Still needs work to parallelise.

--- R/ifu_cube.R
+++ R/ifu_cube.R
@@ -2,10 +2,10 @@
 #'Creating a mock IFU kinematic data cube.
 #'
 #'The purpose of this function is to construct an IFU data cube. It accepts output parameters from
-#' \code{obs_data_prep()} and returns a 3D array containing the spatial and velocity corrdinates of
+#' \code{obs_data_prep()} and \code{img_grid()} and returns a 3D array containing the spatial and velocity corrdinates of
 #'  each particle in the galaxy simulation in order to mimic the arrangement of an IFU data cube.
 #'@param obs_data The list output from the \code{obs_data_prep()} function.
-#'@param threshold The magnitude limit of the observation.
+#'@param img_grid The list output from the \code{img_grid()} function.
 #'@return Returns a list that contains:
 #' \item{\code{$cube}}{A mock IFU data cube as required for calculating the observed spin parameter}
 #' \item{\code{$xbin_labels}}{Bin labels for the x-spatial dimension.}
@@ -22,31 +22,97 @@
 #' cube        = ifu_cube(obs_data  = data, threshold = 20)
 #'
 
-ifu_cube = function(obs_data) {
+ifu_cube = function(obs_data, img_data) {
+  tic("Setup")
+  numCores     = parallel::detectCores()
+  image_grid   = img_data$image_grid
+  lengths_grid = lapply(image_grid, length)
 
-  galaxy_obs      = obs_data$galaxy_obs                                                         # data to populate the aperture
-  sbin            = obs_data$sbin                                                               # number of spatial bins along the x and z axes
-  pixel_sscale    = obs_data$pixel_sscale                                                       # size of spatial bins
-  sbin_breaks     = seq(-(sbin * obs_data$sbinsize) / 2,
-                        (sbin * obs_data$sbinsize) / 2, by=obs_data$sbinsize)                   # spatial bin break positions
-  vbin            = obs_data$vbin                                                               # number of velocity bins
-  vbinsize        = obs_data$vbinsize                                                           # size of velocity bins
-  vseq            = seq(-(vbin * vbinsize) / 2, (vbin * vbinsize) / 2, by=vbinsize)             # velocity bin break positions
-  lsf_size        = obs_data$lsf_size                                                           # size of line spread function fwhm
+  galaxy_obs   = obs_data$galaxy_obs
+  sbin         = obs_data$sbin
+  sbinsize     = obs_data$sbinsize
+  sseq         = seq(-(sbin * sbinsize) / 2, (sbin * sbinsize) / 2, by=sbinsize) # spatial bin break positions
+  xbins        = levels(cut(obs_data$galaxy_obs$x, breaks=sseq))
+  zbins        = levels(cut(obs_data$galaxy_obs$z_obs, breaks=sseq)) # spatial/velocity bins boundaries
+
+  vbin         = obs_data$vbin
+  vbinsize     = obs_data$vbinsize
+  vseq         = seq(-(vbin * vbinsize) / 2, (vbin * vbinsize) / 2, by=vbinsize) # velocity bin break positions
+  vbins        = levels(cut(obs_data$galaxy_obs$vy_obs, breaks=vseq))
+
+  xbin_ls      = rowMeans(cbind(as.numeric(sub("\\((.+),.*", "\\1", xbins)), as.numeric(sub("[^,]*,([^]]*)\\]", "\\1", xbins))))
+  zbin_ls      = rowMeans(cbind(as.numeric(sub("\\((.+),.*", "\\1", zbins)), as.numeric(sub("[^,]*,([^]]*)\\]", "\\1", zbins))))
+  vbin_ls      = rowMeans(cbind(as.numeric(sub("\\((.+),.*", "\\1", vbins)), as.numeric(sub("[^,]*,([^]]*)\\]", "\\1", vbins))))
+
+  lsf_size     = obs_data$lsf_size
+  cube         = array(data = 0, dim = c(sbin, sbin, vbin))
+  ap_cube      = array(data = obs_data$ap_region, dim = c(sbin,sbin,vbin))
+  toc()
+  tic("Loop")
+  for (cell in seq(1, sbin*sbin)){
+    if (lengths_grid[[cell]] > 1){
+      coord = c((cell%/%sbin + 1), cell%%sbin)
+      if (cell%%sbin == 0){coord = c((cell%/%sbin), sbin)}
+      cell_mass = sum(obs_data$galaxy_obs$Mass[image_grid[[cell]]])
+      for (j in 1:lengths_grid[[cell]]){
+        cube[coord[2], coord[1],] = cube[coord[2], coord[1],] +
+          diff((img_data$flux_img[coord[2], coord[1]] *
+                  (obs_data$galaxy_obs$Mass[image_grid[[cell]][j]] / cell_mass)) *
+                 pnorm(vseq, mean=obs_data$galaxy_obs$vy_obs[image_grid[[cell]][j]], sd=lsf_size))
+        # adding the "gaussians" of each particle to the velocity bins
+      }
+    } else {cell = cell + 1}
+  }
+  toc()
+  ## ellipticity calculation ## -------------------------------------------------------------------------------------------------------------------------------
+  tic("Ellipticity")
+  xbin_labels     = expand.grid(matrix(data = rowMeans(cbind(as.numeric(sub("\\((.+),.*", "\\1", xbins)),
+                                                             as.numeric(sub("[^,]*,([^]]*)\\]", "\\1", xbins)))), nrow = sbin, ncol = sbin))
+  zbin_labels     = expand.grid(t(matrix(data = rowMeans(cbind(as.numeric(sub("\\((.+),.*", "\\1", zbins)),
+                                                               as.numeric(sub("[^,]*,([^]]*)\\]", "\\1", zbins)))), nrow = sbin, ncol = sbin)))
+                                                                                                # spatial coordinates in each spaxel in a column for
+                                                                                                #    elliptcity calculation
+  xcen       = .meanwt(xbin_labels, as.vector(img_data$flux_img))                               # calculating the centre of the galaxy
+  zcen       = .meanwt(zbin_labels, as.vector(img_data$flux_img))
+  sx         = sqrt(.varwt(xbin_labels, as.vector(img_data$flux_img), xcen))                    # calculating standard deviation along each direction
+  sz         = sqrt(.varwt(zbin_labels, as.vector(img_data$flux_img), zcen))
+  sxz        = .covarwt(xbin_labels, zbin_labels, as.vector(img_data$flux_img), xcen, zcen)     # calculating the covariance
+  temprad    = .cov2eigval(sx, sz, sxz)                                                         # solving for the eigenvalues to give the major and
+  major      = sqrt(abs(temprad$hi))                                                            #    minor axes lengths
+  minor      = sqrt(abs(temprad$lo))
+  axis_ratio = data.frame("a" = major, "b" = minor)                                             # axis ratio output, kpc
+  toc()
+  output = list("cube"        = cube,
+                "ap_region"   = ap_cube,
+                "xbin_labels" = xbin_ls,
+                "ybin_labels" = zbin_ls,
+                "vbin_labels" = vbin_ls,
+                "axis_ratio"  = axis_ratio)
+
+
+  # galaxy_obs      = obs_data$galaxy_obs                                                         # data to populate the aperture
+  # sbin            = obs_data$sbin                                                               # number of spatial bins along the x and z axes
+  # pixel_sscale    = obs_data$pixel_sscale                                                       # size of spatial bins
+  # sbin_breaks     = seq(-(sbin * obs_data$sbinsize) / 2,
+  #                       (sbin * obs_data$sbinsize) / 2, by=obs_data$sbinsize)                   # spatial bin break positions
+  # vbin            = obs_data$vbin                                                               # number of velocity bins
+  # vbinsize        = obs_data$vbinsize                                                           # size of velocity bins
+  # vseq            = seq(-(vbin * vbinsize) / 2, (vbin * vbinsize) / 2, by=vbinsize)             # velocity bin break positions
+  # lsf_size        = obs_data$lsf_size                                                           # size of line spread function fwhm
   # threshold_flux  = ProSpect::magAB2Jansky(threshold)                                           # calculating threshold flux in units for ifu cube
-  galaxy_obs$binx = cut(galaxy_obs$x, breaks=sbin_breaks, labels=F)
-  galaxy_obs$binz = cut(galaxy_obs$z_obs, breaks=sbin_breaks, labels=F)
-  galaxy_obs$binn = galaxy_obs$binx + (sbin * galaxy_obs$binz) - sbin                           # assigning particles to positions in aperture
+  # galaxy_obs$binx = cut(galaxy_obs$x, breaks=sbin_breaks, labels=F)
+  # galaxy_obs$binz = cut(galaxy_obs$z_obs, breaks=sbin_breaks, labels=F)
+  # galaxy_obs$binn = galaxy_obs$binx + (sbin * galaxy_obs$binz) - sbin                           # assigning particles to positions in aperture
   # xbins           = levels(cut(galaxy_obs$x, breaks=sbin_breaks))
   # zbins           = levels(cut(galaxy_obs$z_obs, breaks=sbin_breaks))                           # spatial/velocity bins boundaries required for the
   #vbins           = levels(cut(galaxy_obs$vy_obs, breaks=vseq))                                 #    galaxy observation
   # xbin_ls         = rowMeans(cbind(as.numeric(sub("\\((.+),.*", "\\1", xbins)), as.numeric(sub("[^,]*,([^]]*)\\]", "\\1", xbins))))
   # zbin_ls         = rowMeans(cbind(as.numeric(sub("\\((.+),.*", "\\1", zbins)), as.numeric(sub("[^,]*,([^]]*)\\]", "\\1", zbins))))
-  vbin_ls         = rowMeans(cbind(as.numeric(sub("\\((.+),.*", "\\1", vbins)), as.numeric(sub("[^,]*,([^]]*)\\]", "\\1", vbins))))
-  ID_grid = vector("list", sbin*sbin)
-  for (i in 1:length(galaxy_obs$ID)){
-    ID_grid[[galaxy_obs$binn[i]]] = append(galaxy_obs$ID[i], ID_grid[[galaxy_obs$binn[i]]])
-  }                                                                                          # calculating the centres of spatial/velocity bins
+  # vbin_ls         = rowMeans(cbind(as.numeric(sub("\\((.+),.*", "\\1", vbins)), as.numeric(sub("[^,]*,([^]]*)\\]", "\\1", vbins))))
+  # ID_grid = vector("list", sbin*sbin)
+  # for (i in 1:length(galaxy_obs$ID)){
+  #   ID_grid[[galaxy_obs$binn[i]]] = append(galaxy_obs$ID[i], ID_grid[[galaxy_obs$binn[i]]])
+  # }                                                                                          # calculating the centres of spatial/velocity bins
   # counts      = with(galaxy_obs, as.integer(binn))                                              # preliminary spatial bin positions of each particle
   # counts_df   = as.data.frame(table(counts))                                                    # numbers of particles in each occupied spatial bin
   # counts_flat = data.frame("counts"=rep(0,sbin*sbin))                                           # a 1D array containing the number of particles in
@@ -115,9 +181,9 @@ ifu_cube = function(obs_data) {
   #               "axis_ratio"  = axis_ratio)
 
 
-  output = list("grid" = ID_grid,
-                "vbins" = vbins,
-                "vbin_labels" = vbin_ls)
+  # output = list("grid" = ID_grid,
+  #               "vbins" = vbins,
+  #               "vbin_labels" = vbin_ls)
 
   return(output)
 
