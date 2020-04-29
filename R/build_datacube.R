af000fa77f389d41b8487c4eb540af4046fbfde7
kateharborne <katherine.harborne@icrar.org>
2020-04-29 13:29:33 +0800

    Fixing overarching function to work with updates to addSky, align, and bin labels.

--- R/build_datacube.R
+++ R/build_datacube.R
@@ -21,12 +21,16 @@
 #'@param inc_deg The inclination at which to observe the galaxy in degrees.
 #'@param filter If Age/Metallicity is supplied, the filter within which the SED is generated.
 #'Options include "r" and "g"  for SDSS-r and SDSS-g bands respectively.
+#'@param align Boolean indicating whether or not to align the semi-major axis with the x-axis.
 #'@param blur \emph{Optional} Specified to apply observational seeing effects to the cube. A list
 #' of the form \code{list("psf" = "Moffat", "fwhm" = 0.5)}. \code{"psf"} specifies the shape of the
 #' PSF chosen and may be either \code{"Moffat"} or \code{"Gaussian"}. \code{"fwhm"} is a numeric
 #' specifying the full-width half-maximum of the PSF given in units of arcseconds.
 #'@return A list containing:
 #' \item{\code{$datacube}}{A 3D array corresponding to the kinematic data cube.}
+#' \item{\code{$xbin_labels}}{Bin labels for the x-spatial dimension.}
+#' \item{\code{$zbin_labels}}{Bin labels for the z-spatial dimension.}
+#' \item{\code{$vbin_labels}}{Bin labels for the velocity dimension.}
 #'@examples
 #' galaxy_data = sim_data(system.file("extdata", 'SimSpin_example.hdf5', package="SimSpin"))
 #' cube = build_datacube(simdata      = galaxy_data,
@@ -42,30 +46,38 @@
 
 build_datacube = function(simdata, r200 = 200, z, fov, ap_shape, central_wvl,
                           lsf_fwhm, pixel_sscale, pixel_vscale, inc_deg, filter="g",
-                          blur){
+                          blur, align=FALSE){
 
   if (missing(blur)) {                                     # IF spatial blurring IS NOT requested
 
-    observe_data = obs_data_prep(simdata, r200, z, fov, ap_shape, central_wvl, lsf_fwhm,
-                                 pixel_sscale, pixel_vscale, inc_deg) # prep simulation data in observer units
+    observe_data = obs_data_prep(simdata = simdata, r200 = r200, z = z, fov = fov, ap_shape = ap_shape,
+                                 central_wvl = central_wvl, lsf_fwhm = lsf_fwhm, pixel_sscale = pixel_sscale,
+                                 pixel_vscale = pixel_vscale, inc_deg = inc_deg, align = align) # prep simulation data in observer units
     fluxes = flux_grid(obs_data = observe_data, filter = filter)
     ifu_imgs = ifu_cube(obs_data = observe_data, flux_data = fluxes) # construct IFU data cube
 
-    output       = list("datacube"     = ifu_imgs$cube)
+    output       = list("datacube"     = ifu_imgs$cube,
+                        "xbin_labels"  = ifu_imgs$xbin_labels,
+                        "zbin_labels"  = ifu_imgs$zbin_labels,
+                        "vbin_labels"  = ifu_imgs$vbin_labels)
 
     return(output)
 
   } else {                                                 # IF spatial blurring IS requested
 
-    observe_data = obs_data_prep(simdata, r200, z, fov, ap_shape, central_wvl, lsf_fwhm,
-                                 pixel_sscale, pixel_vscale, inc_deg) # prep simulation data in observer units
+    observe_data = obs_data_prep(simdata = simdata, r200 = r200, z = z, fov = fov, ap_shape = ap_shape,
+                                 central_wvl = central_wvl, lsf_fwhm = lsf_fwhm, pixel_sscale = pixel_sscale,
+                                 pixel_vscale = pixel_vscale, inc_deg = inc_deg, align = align) # prep simulation data in observer units
     fluxes = flux_grid(obs_data = observe_data, filter = filter)
     ifu_imgs = ifu_cube(obs_data = observe_data, flux_data = fluxes) # construct IFU data cube
     blur_imgs = blur_cube(obs_data = observe_data, ifu_datacube = ifu_imgs, psf = blur$psf,
                              fwhm = blur$fwhm)
                                                            # blur IFU data cube
 
-    output       = list("datacube"     = blur_imgs$cube)
+    output       = list("datacube"     = blur_imgs$cube,
+                        "xbin_labels"  = ifu_imgs$xbin_labels,
+                        "zbin_labels"  = ifu_imgs$zbin_labels,
+                        "vbin_labels"  = ifu_imgs$vbin_labels)
     return(output)
 
   }
