77120cff5a48e59e52fe3fc5791526c3bbe246cd
Kate Harborne <katherine.harborne@icrar.org>
2020-10-23 09:33:59 +0800

    Creating classes for observing_strategy and telescopes.

---
+++ R/observing_strategy.R
@@ -0,0 +1,31 @@
+# Kate Harborne - 16/10/2020
+# SimSpin v2.0.0 - observing_strategy class
+
+observing_strategy = function(z = 0.1, inc_deg = 70, blur = F, fwhm=2, psf="Gaussian"){
+
+  if(blur){
+    if(psf == "Gaussian" | psf == "gaussian"){
+      output = list(z       = z,
+                    inc_deg = inc_deg,
+                    blur    = T,
+                    fwhm    = fwhm,
+                    psf     = "Gaussian")
+    }
+    if(psf == "Moffat" | psf == "moffat"){
+      output = list(z       = z,
+                    inc_deg = inc_deg,
+                    blur    = T,
+                    fwhm    = fwhm,
+                    psf     = "Moffat")
+    }
+  } else {
+    output = list(z       = z,
+                  inc_deg = inc_deg,
+                  blur    = F)
+  }
+
+  class(output) <- "observing_strategy"
+
+  return(output)
+
+}
