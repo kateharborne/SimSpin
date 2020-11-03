cb240016f77c343084e3291733f932e22e98ce1c
Kate Harborne <katherine.harborne@icrar.org>
2020-11-03 11:32:29 +0800

    Updating tests to reflect addition of S/N parameter.

--- tests/testthat/test_build_datacube.R
+++ tests/testthat/test_build_datacube.R
@@ -9,17 +9,18 @@ ss_eagle = system.file("extdata", "SimSpin_example_EAGLE.hdf5", package = "SimSp
 temp_loc = tempdir()
 make_simspin_file(ss_eagle, output = paste(temp_loc, "spectra.fst", sep=""))
 
-# Testing that build_datacube will run
-test_that("Initial run of build_datacube function with defaults #1.", {
-  expect_vector(build_datacube(simspin_file = paste(temp_loc, "spectra.fst", sep=""), telescope = telescope(type="SAMI"),
-                               observing_strategy = observing_strategy(z = 0.05, inc_deg = 45)),
+# Testing that build_datacube works
+test_that("Initial run of build_datacube function #1.", {
+  expect_vector(build_datacube(simspin_file = paste(temp_loc, "spectra.fst", sep=""),
+                               telescope = telescope(type="IFU", lsf_fwhm = 3.6, signal_to_noise = 3),
+                               observing_strategy = observing_strategy(z = 0.05, inc_deg = 45, blur = T)),
                 ptype=list(), size = 2)
 })
 
-test_that("Initial run of build_datacube function with defaults #2.", {
-  expect_vector(build_datacube(simspin_file = paste(temp_loc, "spectra.fst", sep=""), telescope = telescope(type="MaNGA"),
-                               observing_strategy = observing_strategy(z = 0.05, inc_deg = 45, blur = T)),
-                ptype=list(), size = 2)
+# Testing that build_datacube will give warning if the spectra given is low res
+test_that("build_datacube issues warning when spectral resolution < LSF fwhm.", {
+  expect_warning(build_datacube(simspin_file = paste(temp_loc, "spectra.fst", sep=""), telescope = telescope(type="SAMI"),
+                               observing_strategy = observing_strategy(z = 0.05, inc_deg = 45)))
 })
 
 unlink(paste(temp_loc, "spectra.fst", sep=""))
