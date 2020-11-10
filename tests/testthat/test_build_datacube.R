00cd95a9d079b7d14cd1ad82eaba22154e22cbb7
Kate Harborne <katherine.harborne@icrar.org>
2020-11-10 16:33:17 +0800

    Modifying build_datacube to work with the new simspin file format.

--- tests/testthat/test_build_datacube.R
+++ tests/testthat/test_build_datacube.R
@@ -29,3 +29,38 @@ test_that("build_datacube issues warning when spectral resolution < LSF fwhm.", 
 })
 
 unlink(paste(temp_loc, "spectra.fst", sep=""))
+
+# Testing that the spectral_weights functions work as expected
+test_that("spectra are weighted by mass correctly", {
+  weights = list(V5389 = c(7, 8, 0.5117805, 0.4882195, 39, 40, 0.6736590, 0.3263410),
+                 V5464 = c(6, 7, 0.09855354,  0.90144646, 40, 41, 0.79954516, 0.20045484))
+  mass = c(0.0001274539, 0.0001326889)
+  spectra = matrix(data=NA, nrow=2, ncol=53689)
+  Template = ProSpect::EMILES
+  for (i in 1:2){
+    w = weights[[i]]; m = mass[i]
+    spectra[i,] = ((Template$Zspec[[w[2]]][w[6],] * w[4]*w[8]) +
+                   (Template$Zspec[[w[2]]][w[5],] * w[4]*w[7]) +
+                   (Template$Zspec[[w[1]]][w[6],] * w[3]*w[8]) +
+                   (Template$Zspec[[w[1]]][w[5],] * w[3]*w[7])) * 1e10 * m
+
+  }
+  expect_equal(.compute_spectra(weights, mass, Template), spectra, tolerance = 1e-6)
+})
+
+# Testing that the velocity shift functions work as expected
+test_that("velocity shift for wavelengths work correctly", {
+  wavelength = Template$Wave
+  velocity_los = c(27.04932, 40.94573)
+  wave = matrix(data = rep(wavelength, length(velocity_los)), nrow = length(velocity_los), byrow=T)
+  wave_shift = ((velocity_los / .speed_of_light) * wave) + wave # using doppler formula to compute the shift in wavelengths cause by LOS velocity
+
+  wave_shift_comp = matrix(data=NA, nrow=2, ncol=53689)
+
+  for (i in 1:2){
+    wave_shift_comp[i,] = ((velocity_los[i] / .speed_of_light) * wave[i,]) + wave[i,]
+  }
+
+  expect_equal(wave_shift, wave_shift_comp)
+
+})
