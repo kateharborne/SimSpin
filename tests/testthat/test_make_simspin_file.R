924dc62d02aa78f0e2a4b7f0d450dedf885839f5
Kate Harborne <katherine.harborne@icrar.org>
2020-12-03 14:13:51 +0800

    Adding and creatin new tests for new make_simspin_file() and utilities functions.

--- tests/testthat/test_make_simspin_file.R
+++ tests/testthat/test_make_simspin_file.R
@@ -13,47 +13,40 @@ temp_loc = tempdir()
 
 # Test that they the function runs successfully without error
 test_that("Initial run of each simulation type - Gadget.", {
-  skip_on_travis()
-  expect_null(make_simspin_file(ss_gadget, output = paste(temp_loc, "gadget_test_spectra.fst", sep="")))
+  expect_null(make_simspin_file(ss_gadget, output = paste(temp_loc, "gadget_test", sep="")))
 })
 
 test_that("Initial run of each simulation type - HDF5", {
-  skip_on_travis()
-  expect_null(make_simspin_file(ss_hdf5, output = paste(temp_loc, "hdf5_test_spectra.fst", sep="")))
+  expect_null(make_simspin_file(ss_hdf5, output = paste(temp_loc, "hdf5_test", sep="")))
 })
 
 test_that("Initial run of each simulation type - EAGLE", {
-  skip_on_travis()
-  expect_null(make_simspin_file(ss_eagle, output = paste(temp_loc, "eagle_test_spectra.fst", sep="")))
+  expect_null(make_simspin_file(ss_eagle, output = paste(temp_loc, "eagle_test", sep="")))
 })
 
 # Test that the function fails when the file already exists
 test_that("Error when output file already exists and overwrite = F - Gadget",{
-  skip_on_travis()
-  expect_error(make_simspin_file(ss_gadget, output = paste(temp_loc, "gadget_test_spectra.fst", sep="")))
+  expect_error(make_simspin_file(ss_gadget, output = paste(temp_loc, "gadget_test", sep="")))
   })
 
 test_that("Error when output file already exists and overwrite = F - HDF5",{
-  skip_on_travis()
-  expect_error(make_simspin_file(ss_hdf5, output = paste(temp_loc, "hdf5_test_spectra.fst", sep="")))
+  expect_error(make_simspin_file(ss_hdf5, output = paste(temp_loc, "hdf5_test", sep="")))
 })
 
 test_that("Error when output file already exists and overwrite = F - EAGLE",{
-  skip_on_travis()
-  expect_error(make_simspin_file(ss_eagle, output = paste(temp_loc, "eagle_test_spectra.fst", sep="")))
+  expect_error(make_simspin_file(ss_eagle, output = paste(temp_loc, "eagle_test", sep="")))
 })
 
 # Test that the function fails when the template provided is unsupported
 test_that("Error when template is unsupported", {
-  expect_error(make_simspin_file(ss_eagle, template = "abcd", output = paste(temp_loc, "eagle_test_spectra.fst", sep="")))
+  expect_error(make_simspin_file(ss_eagle, template = "abcd"))
 })
 
 # Test that the function will overwrite if overwrite = T
 test_that("Test that old files can be overwritten", {
-  skip_on_travis()
-  expect_null(make_simspin_file(ss_eagle, output = paste(temp_loc, "eagle_test_spectra.fst", sep=""),
+  expect_null(make_simspin_file(ss_gadget, output = paste(temp_loc, "gadget_test", sep=""),
                                  overwrite = T))
 })
 
-unlink(c(paste(temp_loc, "gadget_test_spectra.fst", sep=""), paste(temp_loc, "hdf5_test_spectra.fst", sep=""),
-         paste(temp_loc, "eagle_test_spectra.fst", sep="")))
+unlink(c(paste(temp_loc, "gadget_test", sep=""), paste(temp_loc, "hdf5_test", sep=""),
+         paste(temp_loc, "eagle_test", sep="")))
