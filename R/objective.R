8e419cb28a5de5d6fcff2b30b7a2f43beef7cd49
Kate Harborne <katherine.harborne@icrar.org>
2022-01-20 14:41:46 +0800

    Modifying how new inputs (distance, pointing and pointing_unit) are treated within the function. Updating docs.

--- R/objective.R
+++ R/objective.R
@@ -8,16 +8,9 @@
 #'The purpose of this function is to generate a class that describes the
 #'conditions of the observation for \code{build_datacube()}.
 #'
-#'@param distance Numeric describing the distance to which the observed galaxy
-#' is projected. Default is 0.1 in units of redshift "z". Avaliable unit
-#' options include `distance_unit = 'z'` (redshift distance),
-#' `distance_unit = 'Mpc'`  (physical distance in units of mega-parsec) and
-#' `distance_unit = 'kpc/arcsec'` (angular size in kilo-parsec/arcsecond).
-#'@param distance_units Character describing the type of units used for the
-#' distance parameter. Default is `z` for distances given in units of redshift.
-#' Avaliable options include `z` (redshift distance), `Mpc` (physical distance
-#' in units of mega-parsec) and `kpc/pix` (physical size of each pixel in kpc
-#' i.e. kilo-parsec). Input is NOT case sensitive.
+#'@param distance An object of the class Distance. Includes the measured
+#' distance to the object in a variety of possible units. See
+#' \link[SimSpin::Distance]{Distance()} for further details.
 #'@param inc_deg Numeric describing the projected inclination of the observed
 #' galaxy relative to the z-axis - 0 deg places the galaxy face-on, 90 deg is
 #' edge-on aligned with the horizontal axis. Default is 70.
@@ -27,12 +20,12 @@
 #'@param pointing Numeric array `c(x,y)`. Two elements specifying the position
 #' at which the observation is centred given as a shift relative to the centre.
 #' Can be specified in either physical kilo-parsec offsets
-#' (`pointing_unit = 'kpc'`) or in an angular reference frame using RA and DEC
-#' (`pointing_unit = 'RA/DEC'`)
+#' (`pointing_unit = 'kpc'`) or in an angular reference frame using degrees
+#' (`pointing_unit = 'deg'`)
 #'@param pointing_unit Character describing the units of the pointing. Options
 #' include either physical kilo-parsec offsets `kpc` or an angular reference
-#' frame using RA and DEC `RA/DEC`. All offsets are defined relative to the
-#' centre of the galaxy. Input is NOT case sensitive.
+#' frame using angular offsets in degrees `deg`. All offsets are defined
+#' relative to the centre of the galaxy. Input is NOT case sensitive.
 #'@param blur Boolean describing whether seeing conditions should be applied.
 #' Default is FALSE.
 #'@param fwhm If \code{blur = TRUE}, a numeric that describes the full-width
@@ -47,63 +40,45 @@
 #'conditions = objective()
 #'
 
-objective = function(distance = 0.1, distance_unit = "z",
+objective = function(distance = Distance(z=0.3),
                      inc_deg = 70, twist_deg = 0,
-                     pointing = c(0,0), pointing_unit ="kpc",
+                     pointing = c(0,0), pointing_unit = "kpc",
                      blur = F, fwhm=2, psf="Gaussian"){
 
-  # checks for validity
-  if (stringr::str_to_upper(distance_unit) != "Z" & stringr::str_to_upper(distance_unit) != "MPC" & stringr::str_to_upper(distance_unit) != "KPC/PIX"){
-    stop("Error: Distance unit is not supported. \n Please specify distance_unit = 'z' OR 'Mpc' OR 'kpc/pix'.")
+  if (stringr::str_to_upper(pointing_unit) != "KPC" & stringr::str_to_upper(pointing_unit) != "DEG"){
+    stop("Error: Unable to generate requested Pointing. \n
+         Please specify ONE of the following: pointing_unit = 'kpc' OR 'deg'.")
   }
-  if (stringr::str_to_upper(distance_unit) == "Z" & distance <= 0){
-    stop("Error: Unable to process supplied redshift, z <= 0. \n Please specify z > 0 and try again.")
-  }
-  if (stringr::str_to_upper(distance_unit) == "MPC" & distance < 0){
-    stop("Error: Unable to process supplied distance to system < 0 Mpc. \n Please specify a distance >= 0 Mpc and try again.")
-  }
-  if (stringr::str_to_upper(distance_unit) == "KPC/PIX" & distance <= 0){
-    stop("Error: Unable to process supplied pixel scale =< 0 kpc/pix. \n Please specify a pixel scale > 0 kpc/pix and try again.")
-  }
-  if (stringr::str_to_upper(pointing_unit) != "KPC" & stringr::str_to_upper(pointing_unit) != "RA/DEC"){
-    stop("Error: Pointing unit is not supported. \n Please specify pointing_unit = 'kpc' OR 'RA/DEC'.")
-  }
-
 
   if (blur){
     if(stringr::str_to_upper(psf) != "GAUSSIAN" & stringr::str_to_upper(psf) != "MOFFAT"){
-      stop("Error: Unable to generate requested PSF. \n Please specify psf = 'Gaussian' or 'Moffat'.")
+      stop("Error: Unable to generate requested PSF. \n
+           Please specify ONE of the following: psf = 'Gaussian' OR 'Moffat'.")
     }
 
     if (stringr::str_to_upper(psf) == "GAUSSIAN"){
       output = list(distance       = distance,
-                    distance_unit  = distance_unit,
                     inc_deg        = inc_deg,
                     twist_deg      = twist_deg,
-                    pointing       = pointing,
-                    pointing_unit  = pointing_unit,
+                    pointing       = if (stringr::str_to_upper(pointing_unit) == "KPC") {Pointing(xy_kpc = pointing, distance = distance)} else {Pointing(xy_deg = pointing, distance = distance)},
                     blur           = T,
                     fwhm           = fwhm,
                     psf            = "Gaussian")
     }
     if (stringr::str_to_upper(psf) == "MOFFAT"){
       output = list(distance       = distance,
-                    distance_unit  = distance_unit,
                     inc_deg        = inc_deg,
                     twist_deg      = twist_deg,
-                    pointing       = pointing,
-                    pointing_unit  = pointing_unit,
+                    pointing       = if (stringr::str_to_upper(pointing_unit) == "KPC") {Pointing(xy_kpc = pointing, distance = distance)} else {Pointing(xy_deg = pointing, distance = distance)},
                     blur           = T,
                     fwhm           = fwhm,
                     psf            = "Moffat")
     }
   } else {
     output = list(distance       = distance,
-                  distance_unit  = distance_unit,
                   inc_deg        = inc_deg,
                   twist_deg      = twist_deg,
-                  pointing       = pointing,
-                  pointing_unit  = pointing_unit,
+                  pointing       = if(stringr::str_to_upper(pointing_unit) == "KPC"){Pointing(xy_kpc = pointing, distance = distance)}else{Pointing(xy_deg = pointing, distance = distance)},
                   blur           = F)
   }
 
