11ebd2d7edf508ab21f9f6b8129d532204777305
Kate Harborne <katherine.harborne@icrar.org>
2022-01-21 16:57:21 +0800

    Renaming and re-organising observing_strategy to objective. This new function allows you to specify just one distance measure and one poining measure as input and returns all possible parameters. Changes include that "z" is now called "dist_z" to make clear that this redshift is a "projected distance" rather than a time property.

--- R/objective.R
+++ R/objective.R
@@ -8,24 +8,32 @@
 #'The purpose of this function is to generate a class that describes the
 #'conditions of the observation for \code{build_datacube()}.
 #'
-#'@param distance An object of the class Distance. Includes the measured
-#' distance to the object in a variety of possible units. See
-#' \link[SimSpin::Distance]{Distance()} for further details.
+#'@param dist_z The projected distance at which the galaxy is placed in terms of
+#' redshift distance. This parameter is one of three optional ways of describing
+#' the distance to the observed galaxy. Only ONE of these three must be
+#' specified.
+#'@param dist_Mpc The projected distance at which the galaxy is placed in terms
+#' of physical distance in units of Mega-parsec. This parameter is the second of
+#' three optional ways of describing the distance to the observed galaxy. Only
+#' ONE of these three must be specified.
+#'@param dist_kpc_per_arcsec The projected distance at which the galaxy is
+#' placed in terms of angular distance in units of kilo-parsec per arcsecond.
+#' This parameter is the third of three optional ways of describing the distance
+#' to the observed galaxy. Only ONE of these three must be specified.
 #'@param inc_deg Numeric describing the projected inclination of the observed
 #' galaxy relative to the z-axis - 0 deg places the galaxy face-on, 90 deg is
 #' edge-on aligned with the horizontal axis. Default is 70.
 #'@param twist_deg Numeric describing the viewer's orientation relative to the
 #' x-axis - 0 deg places the galaxy face-on, 90 deg places the galaxy edge-on
 #' aligned with the vertical axis. Default is 0.
-#'@param pointing Numeric array `c(x,y)`. Two elements specifying the position
-#' at which the observation is centred given as a shift relative to the centre.
-#' Can be specified in either physical kilo-parsec offsets
-#' (`pointing_unit = 'kpc'`) or in an angular reference frame using degrees
-#' (`pointing_unit = 'deg'`)
-#'@param pointing_unit Character describing the units of the pointing. Options
-#' include either physical kilo-parsec offsets `kpc` or an angular reference
-#' frame using angular offsets in degrees `deg`. All offsets are defined
-#' relative to the centre of the galaxy. Input is NOT case sensitive.
+#'@param pointing_kpc Numeric array `c(x,y)`. Two elements specifying the
+#' position at which the observation is centred given as a shift relative to the
+#' centre at (0,0) in units of kilo-parsecs. This parameter is ONE of two
+#' optional ways of describing this offset. Default is c(0,0) offset.
+#'@param pointing_deg Numeric array `c(x,y)`. Two elements specifying the
+#' position at which the observation is centred given as a shift relative to the
+#' centre at (0,0) in units of degrees. This parameter is ONE of two
+#' optional ways of describing this offset. Default is c(0,0) offset.
 #'@param blur Boolean describing whether seeing conditions should be applied.
 #' Default is FALSE.
 #'@param fwhm If \code{blur = TRUE}, a numeric that describes the full-width
@@ -40,14 +48,29 @@
 #'conditions = objective()
 #'
 
-objective = function(distance = Distance(z=0.3),
+objective = function(dist_z = 0.05, dist_Mpc, dist_kpc_per_arcsec,
                      inc_deg = 70, twist_deg = 0,
-                     pointing = c(0,0), pointing_unit = "kpc",
+                     pointing_kpc = c(0,0), pointing_deg,
                      blur = F, fwhm=2, psf="Gaussian"){
 
-  if (stringr::str_to_upper(pointing_unit) != "KPC" & stringr::str_to_upper(pointing_unit) != "DEG"){
+  if (missing(dist_Mpc) & missing(dist_kpc_per_arcsec)){
+    distance = Distance(z = dist_z)
+  } else if (missing(dist_z) & !missing(dist_Mpc) & missing(dist_kpc_per_arcsec)){
+    distance = Distance(Mpc = dist_Mpc)
+  } else if (missing(dist_z) & missing(dist_Mpc) & !missing(dist_kpc_per_arcsec)){
+    distance = Distance(kpc_per_arcsec = dist_kpc_per_arcsec)
+  } else {
+    stop("Error: Please specify ONE method of projected distance to the observed galaxy. \n
+         Please specify ONE of the following input parameters: 'dist_z' OR 'dist_Mpc' OR 'dist_kpc_per_arcsec'. ")
+  }
+
+  if (missing(pointing_deg)){
+    pointing = Pointing(xy_kpc = pointing_kpc, distance = distance)
+  } else if (missing(pointing_kpc) & !missing(pointing_deg)){
+    pointing = Pointing(xy_deg = pointing_deg, distance = distance)
+  } else {
     stop("Error: Unable to generate requested Pointing. \n
-         Please specify ONE of the following: pointing_unit = 'kpc' OR 'deg'.")
+          Please specify ONE of the following: pointing_kpc OR pointing_deg.")
   }
 
   if (blur){
@@ -60,7 +83,7 @@ objective = function(distance = Distance(z=0.3),
       output = list(distance       = distance,
                     inc_deg        = inc_deg,
                     twist_deg      = twist_deg,
-                    pointing       = if (stringr::str_to_upper(pointing_unit) == "KPC") {Pointing(xy_kpc = pointing, distance = distance)} else {Pointing(xy_deg = pointing, distance = distance)},
+                    pointing       = pointing,
                     blur           = T,
                     fwhm           = fwhm,
                     psf            = "Gaussian")
@@ -69,7 +92,7 @@ objective = function(distance = Distance(z=0.3),
       output = list(distance       = distance,
                     inc_deg        = inc_deg,
                     twist_deg      = twist_deg,
-                    pointing       = if (stringr::str_to_upper(pointing_unit) == "KPC") {Pointing(xy_kpc = pointing, distance = distance)} else {Pointing(xy_deg = pointing, distance = distance)},
+                    pointing       = pointing,
                     blur           = T,
                     fwhm           = fwhm,
                     psf            = "Moffat")
@@ -78,7 +101,7 @@ objective = function(distance = Distance(z=0.3),
     output = list(distance       = distance,
                   inc_deg        = inc_deg,
                   twist_deg      = twist_deg,
-                  pointing       = if(stringr::str_to_upper(pointing_unit) == "KPC"){Pointing(xy_kpc = pointing, distance = distance)}else{Pointing(xy_deg = pointing, distance = distance)},
+                  pointing       = pointing,
                   blur           = F)
   }
 
