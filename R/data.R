521f4adc117545568eda53a8a3a574c08b3a36b2
Kate Harborne <katherine.harborne@icrar.org>
2022-03-24 12:52:56 +0800

    Documenting the filter data files.

---
+++ R/data.R
@@ -0,0 +1,69 @@
+#' u-band SDSS filter responses at a range of wavelengths
+#'
+#' A dataset containing the relative response of the SDSS u-band filter across
+#' the relevent wavelength range in Angstroms.
+#'
+#' @format A data table with 2 variables and 47 observations:
+#' \describe{
+#' \item{wave}{a numeric representing the wavelength at which the relative response is recorded, given in Angstroms}
+#' \item{response}{a numeric representing the relative response of the filter at that wavelength}
+#' }
+#' @source \url{https://classic.sdss.org/dr7/instruments/imager/filters/u.dat}
+#'
+"filt_u_SDSS"
+
+#' g-band SDSS filter responses at a range of wavelengths
+#'
+#' A dataset containing the relative response of the SDSS g-band filter across
+#' the relevent wavelength range in Angstroms.
+#'
+#' @format A data table with 2 variables and 89 observations:
+#' \describe{
+#' \item{wave}{a numeric representing the wavelength at which the relative response is recorded, given in Angstroms}
+#' \item{response}{a numeric representing the relative response of the filter at that wavelength}
+#' }
+#' @source \url{https://classic.sdss.org/dr7/instruments/imager/filters/g.dat}
+#'
+"filt_g_SDSS"
+
+#' r-band SDSS filter responses at a range of wavelengths
+#'
+#' A dataset containing the relative response of the SDSS r-band filter across
+#' the relevent wavelength range in Angstroms.
+#'
+#' @format A data table with 2 variables and 75 observations:
+#' \describe{
+#' \item{wave}{a numeric representing the wavelength at which the relative response is recorded, given in Angstroms}
+#' \item{response}{a numeric representing the relative response of the filter at that wavelength}
+#' }
+#' @source \url{https://classic.sdss.org/dr7/instruments/imager/filters/r.dat}
+#'
+"filt_r_SDSS"
+
+#' i-band SDSS filter responses at a range of wavelengths
+#'
+#' A dataset containing the relative response of the SDSS i-band filter across
+#' the relevent wavelength range in Angstroms.
+#'
+#' @format A data table with 2 variables and 89 observations:
+#' \describe{
+#' \item{wave}{a numeric representing the wavelength at which the relative response is recorded, given in Angstroms}
+#' \item{response}{a numeric representing the relative response of the filter at that wavelength}
+#' }
+#' @source \url{https://classic.sdss.org/dr7/instruments/imager/filters/i.dat}
+#'
+"filt_i_SDSS"
+
+#' z-band SDSS filter responses at a range of wavelengths
+#'
+#' A dataset containing the relative response of the SDSS z-band filter across
+#' the relevent wavelength range in Angstroms.
+#'
+#' @format A data table with 2 variables and 141 observations:
+#' \describe{
+#' \item{wave}{a numeric representing the wavelength at which the relative response is recorded, given in Angstroms}
+#' \item{response}{a numeric representing the relative response of the filter at that wavelength}
+#' }
+#' @source \url{https://classic.sdss.org/dr7/instruments/imager/filters/z.dat}
+#'
+"filt_z_SDSS"
