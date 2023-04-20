b5ce6603cedbcadaaa2f1d2d745ecfc9337736c0
Kate Harborne <katherine.harborne@icrar.org>
2023-04-20 17:23:41 +0800

    Making sure the noise in gas cubes scales with mass (as there is no "flux").

--- R/build_datacube.R
+++ R/build_datacube.R
@@ -464,8 +464,8 @@ build_datacube = function(simspin_file, telescope, observing_strategy,
       output$variance_cube = noise_cube # initialising empty arrays
 
       noise_cube = .add_noise(output$velocity_cube,
-                              sqrt(max(raw_images$flux_image, na.rm=T))/
-                                (observation$signal_to_noise*sqrt(raw_images$flux_image)))
+                              sqrt(max(raw_images$mass_image, na.rm=T))/
+                                (observation$signal_to_noise*sqrt(raw_images$mass_image)))
       output$velocity_cube = output$velocity_cube + noise_cube
       if (any(output$velocity_cube<0)){
         output$velocity_cube[which(output$velocity_cube<0)] = 0
