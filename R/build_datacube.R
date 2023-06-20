5651c1e837bc71958e631003aa4870f8cc47a24e
Kate Harborne <katherine.harborne@icrar.org>
2023-06-20 07:12:29 +0100

    Ensuring the median flux pixel is not dominated by zeros in low surface brightness regimes by specifying the median must be taken of pixels containing any particles.

--- R/build_datacube.R
+++ R/build_datacube.R
@@ -304,7 +304,7 @@ build_datacube = function(simspin_file, telescope, observing_strategy,
       output$variance_cube = noise_cube # initialising empty arrays
 
       noise_cube = .add_noise(output$spectral_cube,
-                              sqrt(median(raw_images$flux_image, na.rm=T))/
+                              sqrt(median(raw_images$flux_image[raw_images$particle_image > 0], na.rm=T))/
                                 (observation$signal_to_noise*sqrt(raw_images$flux_image)))
 
       output$spectral_cube = output$spectral_cube + noise_cube
@@ -370,7 +370,7 @@ build_datacube = function(simspin_file, telescope, observing_strategy,
       output$variance_cube = noise_cube # initialising empty arrays
 
       noise_cube = .add_noise(output$velocity_cube,
-                              sqrt(median(raw_images$flux_image, na.rm=T))/
+                              sqrt(median(raw_images$flux_image[raw_images$particle_image > 0], na.rm=T))/
                                 (observation$signal_to_noise*sqrt(raw_images$flux_image)))
       noise_image = output$observed_images$flux_image*(rowSums(noise_cube, dims=2)/rowSums(output$velocity_cube, dims=2))
 
@@ -477,7 +477,7 @@ build_datacube = function(simspin_file, telescope, observing_strategy,
       output$variance_cube = noise_cube # initialising empty arrays
 
       noise_cube = .add_noise(output$velocity_cube,
-                              sqrt(median(raw_images$mass_image, na.rm=T))/
+                              sqrt(median(raw_images$mass_image[raw_images$particle_image > 0], na.rm=T))/
                                 (observation$signal_to_noise*sqrt(raw_images$mass_image)))
       noise_image = output$observed_images$flux_image*(rowSums(noise_cube, dims=2)/rowSums(output$velocity_cube, dims=2))
 
