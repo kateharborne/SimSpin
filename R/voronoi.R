13dad160c6b35d716fd0c9bf698a5a4b8ff7b427
Kate Harborne <katherine.harborne@icrar.org>
2023-09-19 09:48:20 +0800

    Functions to label particle grid (part_in_spaxel) with voronoi bins up to some particle number minimum.

--- R/voronoi.R
+++ R/voronoi.R
@@ -1,19 +1,30 @@
 # Kate Harborne 20/09/21
 # Voronoi tessellation of pixels to meet some minimum particle number threshold
 
-.make_bin = function(part_in_spaxel, each, bin_id, verbose){
+.make_bin = function(vorbin_spaxel, bin_id, verbose, index = NULL){
 
     # define some flags for whether or not a bin can accrete any more pixels
     topo_criteria = F
     roundness_criteria = F
     uniform_criteria = F
 
-    # comupte reff, where reff is the radius of a circle with the same area as the whole bin
-    reff = sqrt((part_in_spaxel$area[each] + 1) / pi) # need to join bin to at least one pixel (min)
+    if (is.null(index)){ # if this is the first call of make bin per round, start with all unbinned
+      data.table::setorder(vorbin_spaxel, -N)
+      index_to_bin = which(vorbin_spaxel$pixel_pos == vorbin_spaxel$pixel_pos[vorbin_spaxel$bin_number == 0 &
+                                                                                !is.na(vorbin_spaxel$bin_number)][1])
+      unbinned_spaxels = vorbin_spaxel[vorbin_spaxel$bin_number == 0,]
+      unbinned_spaxels = unbinned_spaxels[-which(unbinned_spaxels$pixel_pos == vorbin_spaxel$pixel_pos[index_to_bin]),]
 
-    unbinned_spaxels = part_in_spaxel[(each+1):total_pixels,]
-    unbinned_spaxels$xbin_cen = unbinned_spaxels$xbin - part_in_spaxel$xbin[each]
-    unbinned_spaxels$ybin_cen = unbinned_spaxels$ybin - part_in_spaxel$ybin[each]
+    } else { # if this is a repeat, consider the old index as the bin to grow
+      index_to_bin = index
+      unbinned_spaxels = vorbin_spaxel[vorbin_spaxel$bin_number == 0,]
+    }
+
+    # comupte reff, where reff is the radius of a circle with the same area as the whole bin
+    reff = sqrt((vorbin_spaxel$area[index_to_bin] + 1) / pi) # need to join bin to at least one pixel (min)
+
+    unbinned_spaxels$xbin_cen = unbinned_spaxels$xbin - vorbin_spaxel$xbin[index_to_bin]
+    unbinned_spaxels$ybin_cen = unbinned_spaxels$ybin - vorbin_spaxel$ybin[index_to_bin]
     unbinned_spaxels$rad = sqrt(unbinned_spaxels$xbin_cen^2 + unbinned_spaxels$ybin_cen^2)
 
     possible_candidates = unbinned_spaxels[unbinned_spaxels$rad <= sqrt(2),] # do we meet the topological criterion?
@@ -37,9 +48,9 @@
 
       if (roundness_criteria){ # if we meet the roundness criteria, test for uniformity
 
-        possible_candidates$uniformity = abs(1 - ((possible_candidates$N + part_in_spaxel$N[each])/observation$particle_limit))
+        possible_candidates$uniformity = abs(1 - ((possible_candidates$N + vorbin_spaxel$N[index_to_bin])/observation$particle_limit))
 
-        possible_candidates = possible_candidates[which(possible_candidates$uniformity <= 0.2),]
+        #possible_candidates = possible_candidates[which(possible_candidates$uniformity <= 0.2),]
 
         if (length(possible_candidates$pixel_pos) >= 1){ # do we meet uniformity?
           uniform_criteria = T
@@ -53,40 +64,45 @@
 
     if (topo_criteria && roundness_criteria && uniform_criteria){ # if we find a suitable pixel to add to the bin
 
-    id_join = which(part_in_spaxel$pixel_pos == join_bin$pixel_pos) # joining pixel
+    id_join = which(vorbin_spaxel$pixel_pos == join_bin$pixel_pos) # joining pixel
 
     # Having identified a pixel to join, let's add the particles to that bin
-    part_in_spaxel$bin_number[each] = bin_id
+    vorbin_spaxel$bin_number[index_to_bin] = bin_id
 
     # which particles are in the bin?
-    part_in_bin = c(part_in_spaxel$val[[each]], part_in_spaxel$val[[id_join]])
-    part_in_spaxel$val[[each]] = part_in_bin # particles in bin updated
+    part_in_bin = c(vorbin_spaxel$val[[index_to_bin]], vorbin_spaxel$val[[id_join]])
+    vorbin_spaxel$val[[index_to_bin]] = part_in_bin # particles in bin updated
 
-    xbin_cen = median(c(part_in_spaxel$xbin[each], part_in_spaxel$xbin[id_join]))
-    ybin_cen = median(c(part_in_spaxel$ybin[each], part_in_spaxel$ybin[id_join]))
+    # record ids of merged pixels
+    vorbin_spaxel$joined_ids[index_to_bin][[1]] =
+      c(vorbin_spaxel$joined_ids[index_to_bin][[1]], vorbin_spaxel$joined_ids[id_join][[1]])
+
+    # update the new number of particles per bin
+    vorbin_spaxel$N[index_to_bin] = sum(vorbin_spaxel$N[index_to_bin], vorbin_spaxel$N[id_join])
+
+    xbin_cen = median(c(vorbin_spaxel$xbin[index_to_bin], vorbin_spaxel$xbin[id_join]))
+    ybin_cen = median(c(vorbin_spaxel$ybin[index_to_bin], vorbin_spaxel$ybin[id_join]))
 
     # updating to the centre of the new bin
-    part_in_spaxel$xbin[each] = xbin_cen
-    part_in_spaxel$ybin[each] = ybin_cen
+    vorbin_spaxel$xbin[index_to_bin] = xbin_cen
+    vorbin_spaxel$ybin[index_to_bin] = ybin_cen
 
     # updating the area of the bin
-    new_area = part_in_spaxel$area[each] + part_in_spaxel$area[id_join]
-    part_in_spaxel$area[each] = new_area
+    new_area = vorbin_spaxel$area[index_to_bin] + vorbin_spaxel$area[id_join]
+    vorbin_spaxel$area[index_to_bin] = new_area
 
-    # then remove the joined pixel from the array
-    part_in_spaxel = part_in_spaxel[-id_join,]
+    # then remove the joined pixel in the array
+    vorbin_spaxel = vorbin_spaxel[-id_join,]
 
     } else { # flag this pixel as unable to join to another
-      part_in_spaxel$bin_number[each] = NA
+      vorbin_spaxel$bin_number[index_to_bin] = NA
     }
 
-    return(part_in_spaxel)
+    return(list("vorbin"=vorbin_spaxel, "index" = index_to_bin))
 
 }
 
 voronoi = function(part_in_spaxel, observation, roundness_limit = 0.3, uniform_limit = 0.8){
-
-  total_pixels = length(part_in_spaxel$pixel_pos)
 
   # Following the algorithm set out in Cappellari & Copin (2003):
   # (i) Begin at the highest valued pixel in the image.
@@ -94,41 +110,114 @@ voronoi = function(part_in_spaxel, observation, roundness_limit = 0.3, uniform_l
   # order by number of particles (max first)
   data.table::setorder(part_in_spaxel, -N)
 
-  # x-y position of pixel
-  part_in_spaxel$xbin = part_in_spaxel$pixel_pos %% observation$sbin
-  part_in_spaxel$ybin = part_in_spaxel$pixel_pos %/% observation$sbin
-  part_in_spaxel$area = 1
-
   # setting "bin_number" value
   part_in_spaxel$bin_number = 0
-  bin_id = 1
 
-  for (each in 1:total_pixels){
+  vorbin_spaxels = part_in_spaxel
+  # x-y position of pixel
+  vorbin_spaxels$xbin = vorbin_spaxels$pixel_pos %% observation$sbin
+  vorbin_spaxels$ybin = vorbin_spaxels$pixel_pos %/% observation$sbin
+  vorbin_spaxels$area = 1
+  vorbin_spaxels$joined_ids = list()
+
+  for (pixel in 1:nrow(vorbin_spaxels)){
+    vorbin_spaxels$joined_ids[pixel][[1]] = c(vorbin_spaxels$pixel_pos[pixel])
+  }
+
+  bin_id = 1
+  each = 1
+
+  # while pixels have not yet been binned, run algorithm
+  while (nrow(vorbin_spaxels[vorbin_spaxels$bin_number == 0]) > 0){
 
     if (part_in_spaxel$N[each] >= observation$particle_limit){
+
       if(verbose){print(paste("Pixel", each, "meets limit - no binning."))}
       part_in_spaxel$bin_number[each] = bin_id
+      vorbin_spaxels$bin_number[each] = bin_id
+
       bin_id = bin_id + 1
+      each = each + 1
+
     } else {
       if(verbose){print(paste("Pixel", each, "does not meet limit - binning commencing."))}
 
-      part_in_spaxel = .make_bin(part_in_spaxel, each, bin_id, verbose)
+      binned_spaxels = .make_bin(vorbin_spaxels, bin_id, verbose, index=NULL)
+      vorbin_spaxels = binned_spaxels$vorbin
+      index = binned_spaxels$index
 
-      while (part_in_spaxel$N[each] < (observation$particle_limit*uniform_limit) &&
-             !is.na(part_in_spaxel$bin_number[each])){
+      while (vorbin_spaxels$N[index] < (observation$particle_limit*uniform_limit) &&
+             !is.na(vorbin_spaxels$bin_number[index])){
         if(verbose){print(paste("More pixels need adding to bin to meet vorbin_limit!"))}
-        part_in_spaxel = .make_bin(part_in_spaxel, each, bin_id, verbose)
+        binned_spaxels = .make_bin(vorbin_spaxels, bin_id, verbose, index = index)
+        vorbin_spaxels = binned_spaxels$vorbin
+        index = binned_spaxels$index
       }
 
-      if(verbose && !is.na(part_in_spaxel$bin_number[each])){print(paste("DONE adding pixels to bin to meet vorbin_limit!"))}
-      if(verbose && is.na(part_in_spaxel$bin_number[each])){print(paste("No pixels available. Flagging pixel and moving on."))}
+      if(!is.na(vorbin_spaxels$bin_number[index])){
+        if(verbose){print(paste("DONE adding pixels to bin to meet vorbin_limit!"))}
+        part_in_spaxel$bin_number[part_in_spaxel$pixel_pos %in% vorbin_spaxels$joined_ids[index][[1]]] =
+          vorbin_spaxels$bin_number[index]
+      }
+
+      if(is.na(vorbin_spaxels$bin_number[index])){
+        if(verbose){print(paste("No pixels available. Flagging pixel and moving on."))}
+        # part_in_spaxel$bin_number[part_in_spaxel$pixel_pos %in% vorbin_spaxels$joined_ids[[index]]] =
+        #   vorbin_spaxels$bin_number[index]
+      }
+
       bin_id = bin_id + 1
-
+      each = each + 1
     }
-
-    readline(prompt=paste("Each is ", each, ". Press [enter] to continue"))
 
   }
 
+  # iterate through pixels unable to be binned and add them to the nearest bin
+  while (any(is.na(vorbin_spaxels$bin_number))){
+
+    if(verbose){
+      print(paste0(length(vorbin_spaxels$bin_number[is.na(vorbin_spaxels$bin_number)]), " unbinned pixels..."))
+    }
+
+    # take a single pixel that has not yet been binned
+    unbinned_spaxel = vorbin_spaxels[is.na(vorbin_spaxels$bin_number),][1]
+    bin_index = which(vorbin_spaxels$pixel_pos == unbinned_spaxel$pixel_pos)
+
+    #compute distance between these and all binned pixels
+    binned_spaxels = vorbin_spaxels[!is.na(vorbin_spaxels$bin_number) & vorbin_spaxels$bin_number > 0,]
+    binned_spaxels$xbin_cen = binned_spaxels$xbin - unbinned_spaxel$xbin
+    binned_spaxels$ybin_cen = binned_spaxels$ybin - unbinned_spaxel$ybin
+    binned_spaxels$rad = sqrt(binned_spaxels$xbin_cen^2 + binned_spaxels$ybin_cen^2)
+
+    join_to_bin = binned_spaxels$bin_number[which(binned_spaxels$rad == min(binned_spaxels$rad))]
+    join_index = which(vorbin_spaxels$bin_number %in% join_to_bin)
+
+    if (length(join_to_bin) > 1){ # if more than 1 close bin, minimise uniformity
+      uniformity = abs(1 - ((binned_spaxels$N[join_index] + unbinned_spaxel$N)/observation$particle_limit))
+      n = which(uniformity == min(uniformity))[1]
+      join_index = join_index[n]
+    } else {n = 1}
+
+    # add unbinned to the closest bin
+    vorbin_spaxels$joined_ids[join_index][[1]] =
+      c(vorbin_spaxels$joined_ids[join_index][[1]], unbinned_spaxel$joined_ids[[1]])
+
+    vorbin_spaxels$N[join_index] = sum(vorbin_spaxels$N[join_index],
+                                       unbinned_spaxel$N)
+
+    vorbin_spaxels$xbin[join_index] = median(vorbin_spaxels$xbin[join_index], unbinned_spaxel$xbin)
+    vorbin_spaxels$ybin[join_index] = median(vorbin_spaxels$ybin[join_index], unbinned_spaxel$ybin)
+
+    vorbin_spaxels$area[join_index] = sum(vorbin_spaxels$area[join_index], unbinned_spaxel$area)
+
+    # update bin number of joined bin
+    vorbin_spaxels = vorbin_spaxels[-bin_index,]
+
+    # update the bin in part_in_spaxel
+    part_in_spaxel$bin_number[part_in_spaxel$pixel_pos %in% vorbin_spaxels$joined_ids[join_index][[1]]] = join_to_bin[n]
+
+  }
+
+  return(part_in_spaxel)
 
 }
