fddf00cba652b06625310ea46a6c71fe1fae3070
Kate Harborne <katherine.harborne@icrar.org>
2025-03-03 16:32:18 +0000

    Ensure H0 is defined for all simulation Types.

--- R/read_files.R
+++ R/read_files.R
@@ -461,6 +461,7 @@
 .magneticum_read_hdf5 = function(data, head, cores){
 
   head$Type = "Magneticum"
+  head$H0 = head$HubbleParam * 100
   names(head)[names(head) == "Omega0"] = "OmegaM"
   names(head)[names(head) == "OmegaLambda"] = "OmegaL"
   head$OmegaR = 0
@@ -575,6 +576,7 @@
 .horizonagn_read_hdf5 = function(data, head, cores){
 
   head$Type = "Horizon-AGN"
+  head$H0 = head$HubbleParam * 100
   names(head)[names(head) == "Omega0"] = "OmegaM"
   names(head)[names(head) == "OmegaLambda"] = "OmegaL"
   head$OmegaR = 0
@@ -695,6 +697,7 @@
 .illustristng_read_hdf5 = function(data, head, cores){
 
   head$Type = "Illustris-TNG"
+  head$H0 = head$HubbleParam * 100
   names(head)[names(head) == "Omega0"] = "OmegaM"
   names(head)[names(head) == "OmegaLambda"] = "OmegaL"
   head$OmegaR = 0
