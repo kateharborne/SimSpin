97dc19f036d9d958dc99171f11bf33dd4c3f33bf
Katherine Harborne <katherine.e.harborne@durham.ac.uk>
2025-04-16 12:52:42 +0100

    Modifying the lower limit offered by optim fit for dispersion to reflect physical prescription of the underlying IFU.

--- R/build_datacube.R
+++ R/build_datacube.R
@@ -490,7 +490,7 @@ build_datacube = function(simspin_file, telescope, observing_strategy,
                                          fn    = .losvd_fit_h3h4,
                                          x     = observation$vbin_seq,
                                          losvd = (output$velocity_cube[c,d,]/(max(output$velocity_cube[c,d,], na.rm=T))),
-                                         method="L-BFGS-B", lower = c(NA, 1e-10, NA, NA),
+                                         method="L-BFGS-B", lower = c(NA, observation$vbin_error/2, NA, NA),
                                          control=list(pgtol=1e-9))$par},
                            error = function(e){c(0,0,0,0)})
 
@@ -514,7 +514,7 @@ build_datacube = function(simspin_file, telescope, observing_strategy,
                                          fn    = .losvd_fit_vsig,
                                          x     = observation$vbin_seq,
                                          losvd = (output$velocity_cube[c,d,]/(max(output$velocity_cube[c,d,], na.rm=T))),
-                                         method="L-BFGS-B", lower = c(NA, 1e-10),
+                                         method="L-BFGS-B", lower = c(NA, observation$vbin_error/2),
                                          control=list(pgtol=1e-9))$par},
                            error = function(e){c(0,0)})
 
@@ -623,7 +623,7 @@ build_datacube = function(simspin_file, telescope, observing_strategy,
                                         fn    = .losvd_fit_h3h4,
                                         x     = observation$vbin_seq,
                                         losvd = (output$velocity_cube[c,d,]/(max(output$velocity_cube[c,d,], na.rm=T))),
-                                        method="L-BFGS-B", lower = c(NA, 0, NA, NA),
+                                        method="L-BFGS-B", lower = c(NA, observation$vbin_error/2, NA, NA),
                                         control=list(pgtol=1e-9))$par},
                           error = function(e){c(0,0,0,0)})
 
@@ -648,7 +648,7 @@ build_datacube = function(simspin_file, telescope, observing_strategy,
                                         fn    = .losvd_fit_vsig,
                                         x     = observation$vbin_seq,
                                         losvd = (output$velocity_cube[c,d,]/(max(output$velocity_cube[c,d,], na.rm=T))),
-                                        method="L-BFGS-B", lower = c(NA, 0),
+                                        method="L-BFGS-B", lower = c(NA, observation$vbin_error/2),
                                         control=list(pgtol=1e-9))$par},
                           error = function(e){c(0,0)})
 
