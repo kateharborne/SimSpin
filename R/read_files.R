2701a7b730e0372beb1e9aeebd21a4d678268ef6
Katherine Harborne <katherine.e.harborne@durham.ac.uk>
2025-04-24 15:48:25 +0100

    Merged changes to match main.

--- R/read_files.R
+++ R/read_files.R
@@ -1146,11 +1146,11 @@
   if (cores > 1) {
     doParallel::registerDoParallel(cores = cores)
     i = integer()
-    output = foreach(i = 1:length(a), .packages = "celestial") %dopar% { cosdist(a[i], H0) }
+    output = foreach(i = 1:length(a), .packages = "celestial") %dopar% { cosdist(a[i]) }
     closeAllConnections()
   }
   else {
-    output = lapply(a, cosdist, Hubble0 = H0)
+    output = lapply(a, cosdist)
   }
   return(output)
 }
