593dccfda20068d63f74a8e8475d0b6804519283
Katherine Harborne <katherine.e.harborne@durham.ac.uk>
2025-10-27 16:50:33 +0000

    Adding a catch for case where the file is not one of the supported types, to point towards a generic file reader.

--- R/read_files.R
+++ R/read_files.R
@@ -170,8 +170,7 @@
       if(stringr::str_detect(stringr::str_to_lower(head$RunLabel), "magneticum")){magneticum = T}else{magneticum=F}
       if(stringr::str_detect(stringr::str_to_lower(head$RunLabel), "horizon")){horizonagn = T}else{horizonagn = F}
       if(stringr::str_detect(stringr::str_to_lower(head$RunLabel), "colibre")){colibre = T}else{colibre = F}
-    }
-
+      if(!all(eagle, illustristng, magneticum, horizonagn, colibre, gadget2)){generic=T}else{generic = F}
   }
 
   # Read particle data differently depending on the simulation being read in...
@@ -181,6 +180,7 @@
   if (horizonagn){output = .horizonagn_read_hdf5(data, head, cores)}
   if (illustristng){output = .illustristng_read_hdf5(data, head, cores)}
   if (colibre){output = .colibre_read_hdf5(data, head, cores)}
+  if (generic){output = .generic_read_hdf5(data, head, cores)}
 
   hdf5r::h5close(data)
 
