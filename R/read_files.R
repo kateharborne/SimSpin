f642c9ee3af0815f1b6f1b19e010359dd29a6d38
Katherine Harborne <katherine.e.harborne@durham.ac.uk>
2025-10-28 09:56:13 +0000

    Returning missing bracket to .read_hdf5()

--- R/read_files.R
+++ R/read_files.R
@@ -171,6 +171,7 @@
       if(stringr::str_detect(stringr::str_to_lower(head$RunLabel), "horizon")){horizonagn = T}else{horizonagn = F}
       if(stringr::str_detect(stringr::str_to_lower(head$RunLabel), "colibre")){colibre = T}else{colibre = F}
       if(!all(eagle, illustristng, magneticum, horizonagn, colibre, gadget2)){generic=T}else{generic = F}
+    }
   }
 
   # Read particle data differently depending on the simulation being read in...
@@ -1261,3 +1262,4 @@
   }
   return(output)
 }
+
